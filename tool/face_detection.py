f3dc85c69fc067b695df42612a8a2184e2f6512c
Thiện Đoàn Nguyễn Chí <chithien422001@gmail.com>
2025-01-15 00:39:27 +0700

    .

---
+++ tool/face_detection.py
@@ -0,0 +1,201 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import time
+
+
+
+
+
+class Face_detector():
+    def __init__(self):
+        mp_face_mesh = mp.solutions.face_mesh
+        self.face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+        
+        mp_drawing = mp.solutions.drawing_utils
+        self.drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+    def face_detect(self, image):
+        image = cv2.cvtColor(cv2.flip(image,1), cv2.COLOR_BGR2RGB)
+    
+        image.flags.writeable = False
+
+        result = self.face_mesh.process(image)
+
+        image.flags.writeable = True
+
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        img_h, img_w, img_c = image.shape
+
+        face_3d = []
+        face_2d = []
+        if len(result.multi_face_landmarks) > 1:
+            return False
+        elif result.multi_face_landmarks:
+            for face_landmarks in result.multi_face_landmarks:
+                for idx, lm in enumerate(face_landmarks.landmark):
+                    if idx == 33 or idx == 263 or idx == 1 or idx == 61 or idx == 291 or idx == 199:
+                        if idx == 1:
+                            nose_2d = (lm.x *img_w, lm.y* img_h)
+                            nose_3d = (lm.x*img_w, lm.y*img_h, lm.z*3000)
+                        x,y = int(lm.x *img_w), (lm.y*img_h)
+                        
+                        face_2d.append([x,y])
+
+                        face_3d.append([x,y,lm.z])
+                
+                face_2d = np.array(face_2d, dtype=np.float64)
+
+                face_3d = np.array(face_3d, dtype=np.float64)
+
+                focal_length = 1*img_w
+
+                cam_matrix = np.array([[focal_length, 0, img_h/2],
+                                    [0, focal_length, img_w/2],
+                                    [0,0,1]])
+                dist_matrix = np.zeros((4,1), dtype=np.float64)
+
+                success, rot_vec, trans_vec = cv2.solvePnP(face_3d, face_2d, cam_matrix, dist_matrix)
+
+                rmat, jac = cv2.Rodrigues(rot_vec)
+
+                angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
+
+                x = angles[0] * 360
+                y = angles[1] * 360
+                z = angles[2] * 360
+                nose_3d_projection, jacobian = cv2.projectPoints(nose_3d, rot_vec, trans_vec, cam_matrix, dist_matrix)
+
+                p1 = (int(nose_2d[0]), int(nose_2d[1]))
+                p2 = (int(nose_2d[0] + y * 10), int(nose_2d[1] - x * 10))
+
+                cv2.line(image, p1, p2, (255,0,0), 3)
+
+                gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                face_classifier = cv2.CascadeClassifier(
+                cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+                face = face_classifier.detectMultiScale(
+                gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40))
+                for (x, y, w, h) in face:
+                    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 4)
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                if y < -10:
+                    
+                    text = "looking left"
+                elif y>10:
+                    
+                    text = "looking right"
+                elif x < -10:
+                    
+                    text = "looking down"
+                elif x > 10:
+                    
+                    text = "looking up"
+                else: text = ""
+                return text, image
+# cap = cv2.VideoCapture(0)
+# while cap.isOpened():
+#     success, image = cap.read()
+#     start = time.time()
+    
+#     image = cv2.cvtColor(cv2.flip(image,1), cv2.COLOR_BGR2RGB)
+    
+#     image.flags.writeable = False
+
+#     result = face_mesh.process(image)
+
+#     image.flags.writeable = True
+
+#     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+#     img_h, img_w, img_c = image.shape
+
+#     face_3d = []
+#     face_2d = []
+
+#     if result.multi_face_landmarks:
+#         for face_landmarks in result.multi_face_landmarks:
+#             for idx, lm in enumerate(face_landmarks.landmark):
+#                 if idx == 33 or idx == 263 or idx == 1 or idx == 61 or idx == 291 or idx == 199:
+#                     if idx == 1:
+#                         nose_2d = (lm.x *img_w, lm.y* img_h)
+#                         nose_3d = (lm.x*img_w, lm.y*img_h, lm.z*3000)
+#                     x,y = int(lm.x *img_w), (lm.y*img_h)
+                    
+#                     face_2d.append([x,y])
+
+#                     face_3d.append([x,y,lm.z])
+            
+#             face_2d = np.array(face_2d, dtype=np.float64)
+
+#             face_3d = np.array(face_3d, dtype=np.float64)
+
+#             focal_length = 1*img_w
+
+#             cam_matrix = np.array([[focal_length, 0, img_h/2],
+#                                    [0, focal_length, img_w/2],
+#                                    [0,0,1]])
+#             dist_matrix = np.zeros((4,1), dtype=np.float64)
+
+#             success, rot_vec, trans_vec = cv2.solvePnP(face_3d, face_2d, cam_matrix, dist_matrix)
+
+#             rmat, jac = cv2.Rodrigues(rot_vec)
+
+#             angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
+
+#             x = angles[0] * 360
+#             y = angles[1] * 360
+#             z = angles[2] * 360
+
+#             if y < -10:
+#                 text = "looking left"
+#             elif y>10:
+#                 text = "looking right"
+#             elif x < -10:
+#                 text = "looking down"
+#             elif x > 10:
+#                 text = "looking up"
+#             else: text = "forward"
+
+#             nose_3d_projection, jacobian = cv2.projectPoints(nose_3d, rot_vec, trans_vec, cam_matrix, dist_matrix)
+
+#             p1 = (int(nose_2d[0]), int(nose_2d[1]))
+#             p2 = (int(nose_2d[0] + y * 10), int(nose_2d[1] - x * 10))
+
+#             cv2.line(image, p1, p2, (255,0,0), 3)
+
+#             # cv2.putText(image, text, (20,50,), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 2)
+#             # cv2.putText(image, "x: "+ str(np.round(x,2)), (500,50), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,255), 2)
+#             # cv2.putText(image, "y: "+ str(np.round(y,2)), (500,100), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,255), 2)
+#             # cv2.putText(image, "z: "+ str(np.round(z,2)), (500,150), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,255), 2)
+#         end = time.time()
+
+#         total_time = end-start
+
+#         fps = 1/total_time
+
+#         print("FPS:", len(result.multi_face_landmarks))
+
+#         # cv2.putText(image, f"FPS:{int(fps)}",(20,450), cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
+
+#         # mp_drawing.draw_landmarks(image=image,
+#         #                           landmark_list = face_landmarks,
+#         #                           connections = mp_face_mesh.FACEMESH_CONTOURS,
+#         #                           landmark_drawing_spec = drawing_spec,
+#         #                           connection_drawing_spec = drawing_spec)
+#     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#     face_classifier = cv2.CascadeClassifier(
+#     cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+#     face = face_classifier.detectMultiScale(
+#     gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40))
+#     for (x, y, w, h) in face:
+#         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 4)
+#     # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#     cv2.imshow("head_pose", image)
+
+#     if cv2.waitKey(5) & 0xFF == 27:
+#         break
+# cap.release()
+
+            
+
--- widget/main_view.py
+++ widget/main_view.py
@@ -8,12 +8,13 @@ from PyQt6.QtWidgets import (
     QVBoxLayout,
     QPushButton,
     QWidget,QLayout,
-    QFileDialog,QMessageBox
+    QFileDialog,QMessageBox,
+    QTextEdit
 )
-
+from tool import face_detection
 from PyQt6.QtGui import QPixmap, QImage, QIcon
 from PyQt6.QtCore import QSize, Qt
-
+import cv2
 class mainView(QMainWindow):
     def __init__(self):
         super().__init__(parent=None)
@@ -22,15 +23,39 @@ class mainView(QMainWindow):
         self.setMinimumSize(*size)
         
         self.total_widget = QWidget()
-        self.centralWidget(self.total_widget)
-        
+        self.setCentralWidget(self.total_widget)
+
+
+        self.detect_face = face_detection.Face_detector()
+        self.cap = cv2.VideoCapture(0)
+
+        self.create_total_widget()
+        self.run()
     def create_total_widget(self):
         name_lb = QLabel(parent=self.total_widget)
-        name_lb.setGeometry(20,20)
-        name_lb.setText("HE THONG HO TRO CANH BAO VI PHAM")
+        name_lb.move(20,20)
+        name_lb.setText("HỆ THỐNG HỖ TRỢ CẢNH BÁO VI PHẠM")
         
         
         self.image_lb = QLabel(parent=self.total_widget)
-        self.image_lb.setGeometry(20,200)
+        self.image_lb.setGeometry(200,200,900,900)
+        
+        self.message = QTextEdit(parent=self.total_widget)
+        self.message.setGeometry(1200,200,500,500)
+
+        self.exit_bt = QPushButton(parent=self.total_widget)
+        self.exit_bt.setText("Thoát")
+        self.exit_bt.move(1200,900)
+    def run(self):
+        while self.cap.isOpened():
+            success, image = self.cap.read()
+            text,image = self.detect_face.face_detect(image)
+            w,h,ch = image.shape
+            bytes_per_line = ch * w
+            convert_to_Qt_format = QImage(image.data, 900, 900, bytes_per_line, QImage.Format.Format_RGB888)
+            self.image_lb.setPixmap(QPixmap.fromImage(convert_to_Qt_format))
+
+        
+
         
         
